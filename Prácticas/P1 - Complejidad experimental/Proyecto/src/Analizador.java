fae6292a1651451d5e29b4a68675443b36a42d24
15Galan <antoniogalanG15@gmail.com>
2021-10-23 20:08:01 +0200

    Práctica 1 modificada
    Se ha retocado un poco del código y los javadocs.
    No aporta funciones nuevas.

--- Prácticas/P1 - Complejidad experimental/Proyecto/src/Analizador.java
+++ Prácticas/P1 - Complejidad experimental/Proyecto/src/Analizador.java
@@ -10,11 +10,11 @@ import java.util.Map.Entry;
 
 
 /**
+ * Obtiene el orden de complejidad de un algoritmo.
  *
- *
- * @author Antonio J. Galán Herrera
+ * @author 	Antonio J. Galán Herrera
+ * @see 	Algoritmo#f(long)
  */
-
 public class Analizador {
 
 	// Valores pequeños para complejidades altas y valores altos para complejidades bajas-medias
@@ -24,23 +24,25 @@ public class Analizador {
 								1000000,1500000,2000000,2500000,3000000,3500000,5000000,7000000,
 								10000000,30000000,50000000,800000000,90000000,
 								100000000,500000000,999999999};
-	private static final String[] complejidades = {"1", "NLOGN", "N", "LOGN", "N2", "N3", "2N", "NF"};
-	private static final double TIEMPO_MAX = 10 * Math.pow(10, 9);	// Tiempo de ejecución que no se debe sobrepasar (nseg)
+	private static final double TIEMPO_MAX = 10 * Math.pow(10, 9);	// Tiempo máximo de ejecución (nano-segundos)
 
+
+	/**
+	 * Ejecuta el programa principal del proyecto.
+	 *
+	 * @param arg	Array de argumentos
+	 */
 	public static void main(String[] arg) {
 		Map<String, Datos> tabla = new TreeMap<>();		// Mapa que relaciona cada complejidad con unos tiempos
 		List<Double> tiempos;							// Tiempos medios de cada array anterior (media/posición)
 		int intentos = 100;								// Número de intentos para sacar una media
 
-//		System.out.println("- INICIO -\n");
-//		System.out.println("n <- " + Arrays.toString(n) + "\n");
 
 		tiempos = calcularTiempos(intentos, TIEMPO_MAX/2);
 
-		for (String complejidad : complejidades) {
-			Datos valor = new Datos(complejidad, adaptarMedias(complejidad, tiempos));
-
-			tabla.put(complejidad, valor);
+		// Guardar tiempos TODO: Modularizar
+		for (String complejidad : new String[]{"1", "NLOGN", "N", "LOGN", "N2", "N3", "2N", "NF"}) {
+			tabla.put(complejidad, new Datos(complejidad, adaptarMedias(complejidad, tiempos)));
 		}
 
 //		Set<Entry<String, Datos>> entradas = tabla.entrySet();
@@ -58,15 +60,15 @@ public class Analizador {
 //		System.out.println("\n- FINAL -\n\n" + String.format("%.3f Seg", (double) ejecucion.tiempoPasado()*Math.pow(10, -9)));
 	}
 
+
 	/**
-	 * Ejecuta el algoritmo varias veces con distintos valores y almacena su
-	 * media de tiempos en una lista; esto solo se llevará a cabo hasta
-	 * que el tiempo de ejecución supere el tiempo límite indicado
+	 * Ejecuta el algoritmo varias veces con distintos valores y almacena su media de tiempos en una lista;
+	 * esto solo se llevará a cabo hasta que el tiempo de ejecución supere el tiempo límite indicado.
 	 *
-	 * @param intentos Veces que el algoritmo se ejecutará para un mismo valor
-	 * @param tiempoMax	Duración aproximada (nseg) de este método (debe ser bastante menor que TIEMPO_MAX)
+	 * @param intentos		Veces que el algoritmo se ejecutará para un mismo valor
+	 * @param tiempoMax		Duración aproximada de este método (nano-segundos); debe ser bastante menor que TIEMPO_MAX
 	 *
-	 * @return Una lista de medias de tiempos de ejecución para cada valor de entrada del algoritmo
+	 * @return	Una lista de medias de tiempos de ejecución para cada valor de entrada del algoritmo
 	 */
 	private static List<Double> calcularTiempos(int intentos, double tiempoMax) {
 		Temporizador acumulado = new Temporizador(2);		// Contará el tiempo de ejecución de este método
@@ -74,10 +76,10 @@ public class Analizador {
 
 		boolean continuar = true;	// Determina si es posible ejecutar el algoritmo una vez más
 		double duracion;			// Tiempo total de ejecución del algoritmo para un valor determinado
-		int i = 0;
 
 		acumulado.iniciar();
 
+		int i = 0;
 		while (i < n.length && continuar) {
 			duracion = 0;
 
@@ -85,7 +87,7 @@ public class Analizador {
 
 			// Calcular el tiempo medio del algoritmo para un valor determinado
 			while (j < intentos && continuar) {
-				duracion += intentar(n[i]);			// Medir el tiempo de ejecución del algoritmo
+				duracion += ejecutar(n[i]);			// Medir el tiempo de ejecución del algoritmo
 
 				if (acumulado.tiempoPasado() > tiempoMax) {
 					continuar = false;
@@ -98,7 +100,7 @@ public class Analizador {
 			if (continuar) {
 				tiempos.add(duracion/intentos);
 
-//				System.out.println("\tT(" + n[i] + ") = " + duracion/intentos * Math.pow(10, -9) + " Seg");
+				System.out.println("\tT(" + n[i] + ") = " + duracion/intentos * Math.pow(10, -9) + " seg");
 			}
 
 			i++;
@@ -108,14 +110,14 @@ public class Analizador {
 	}
 
 	/**
-	 * Realiza una ejecución del algoritmo para un determinado valor
+	 * Ejecuta el algoritmo para un determinado valor.
 	 *
-	 * @param n Valor de entrada del algoritmo
+	 * @param n		Entrada del algoritmo
 	 *
-	 * @return El tiempo que tardó el algoritmo en procesar el valor anterior
+	 * @return	Tiempo que tardó el algoritmo en finalizar la ejecución
 	 */
-	private static long intentar(long n) {
-		Temporizador temporizador = new Temporizador(2);	// Trabajará en modo 2: NANOsegundos
+	private static long ejecutar(long n) {
+		Temporizador temporizador = new Temporizador(2);	// Temporizador en nano-segundos
 
 		temporizador.iniciar();
 		Algoritmo.f(n);
@@ -126,12 +128,12 @@ public class Analizador {
 
 	/**
 	 * Dada una lista de tiempos, divide cada componente entre la función de su
-	 * complejidad: f(1), f(log(n)), f(n), f(n·log(n))... y la almacena en una lista
+	 * complejidad: O(1), O(log(n)), O(n), O(n·log(n))... y la almacena en una lista
 	 *
-	 * @param complejidad Complejidad para la que se quiere adaptar las medias
-	 * @param medias Lista de tiempos medios de ejecución de un algoritmo (nseg)
+	 * @param complejidad	Complejidad para la que se quiere adaptar las medias
+	 * @param medias 		Lista de tiempos medios de ejecución de un algoritmo en nano-segundos
 	 *
-	 * @return La lista inicial adaptada a la complejidad indicada
+	 * @return	La lista inicial adaptada a la complejidad indicada
 	 */
 	private static List<Double> adaptarMedias(String complejidad, List<Double> medias) {
 		List<Double> arregladas = new ArrayList<>();
@@ -199,10 +201,9 @@ public class Analizador {
 	}
 
 	/**
-	 * Dado un mapa cuyas claves son complejidades y sus valores asociados son
-	 * objetos de la clase Datos (que contiene el nombre de la complejidad, sus
-	 * tiempos medios adaptados y su ratio), comprueba cuál de las complejidades
-	 * tiende a un valor constante
+	 * Dado un mapa cuyas claves son complejidades y sus valores asociados son objetos de la clase Datos (que contiene
+	 * el nombre de la complejidad, sus tiempos medios adaptados y su ratio), comprueba cuál de las complejidades
+	 * tiende a un valor constante.
 	 *
 	 * @param complejidades Mapa con todos los datos calculados para cada complejidad
 	 *
