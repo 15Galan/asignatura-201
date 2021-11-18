0409745129b3517e782f2338f54bafa717fda7ae
15Galan <antoniogalanG15@gmail.com>
2021-11-18 14:47:44 +0100

    BuscaElem.java analizado
    Se han modificado y añadido comentarios para
    explicar el funcionamiento del algoritmo en el código.

--- Prácticas/P2 - Ordenación Rápida/Proyecto/src/BuscaElem.java
+++ Prácticas/P2 - Ordenación Rápida/Proyecto/src/BuscaElem.java
@@ -10,31 +10,27 @@ import java.util.Scanner;
 
 
 /**
- * Ordena un vector de elementos de forma ascendente.
+ * Busca un elemento en un vector.
  *
  * @author 		Antonio J. Galán Herrera
  */
 public final class BuscaElem {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		int tam, k;
 
-		Scanner sc = new Scanner(System.in);
-
 		System.out.print("Introduce el número de posiciones del vector: ");
-
 		tam = sc.nextInt();
 
-		Integer[] v = new Integer[tam];
-
 		System.out.print("Introduce " + tam + " enteros separados por espacios: ");
+		Integer[] v = new Integer[tam];
 
 		for (int i = 0; i < tam; i++) {
 			v[i] = sc.nextInt();
 		}
 
 		System.out.print("Introduce la posición k deseada (de 1-" + tam + "): ");
-
 		k = sc.nextInt();
 
 		System.out.print("El elemento " + k + "-esimo es: " + kesimo(v,k-1));
@@ -44,36 +40,39 @@ public final class BuscaElem {
 	 * Devuelve el valor contenido en la posición k-ésima
 	 * de un vector si estuviera ascendentemente ordenado.
 	 *
-	 * @param V 	Vector a ordenar
+	 * @param V 	Vector donde buscar
 	 * @param k 	Posición del valor deseado
 	 * @param <T> 	Tipo del vector
 	 *
-	 * @return 	Valor en la posición k del vector ya ordenado
+	 * @return 	Valor k-ésimo del vector si estuviera ascendentemente ordenado
 	 */
 	public static <T extends Comparable <? super T>> T kesimo(T[] V, int k) {
 		return kesimoRec(V, 0, V.length-1, k);
 	}
 
 	/**
-	 * Busca un valor k-ésimo en un intervalo de un vector
+	 * Busca el valor k-ésimo en un intervalo de un vector
+	 * definido por sus extremos inferior y superior (incluidos).
 	 *
-	 * @param V 	Vector
-	 * @param izq 	Valor inicial del intervalo
-	 * @param der 	Valor final del intervalo
+	 * @param V 	Vector donde buscar
+	 * @param inf 	Límite inferior del intervalo
+	 * @param sup 	Límite superior del intervalo
 	 * @param k 	Posición del elemento deseado
 	 * @param <T> 	Tipo del vector
 	 *
-	 * @return 	Valor contenido en la posición k del vector
+	 * @return 	Valor k-ésimo del intervalo si estuviera ascendentemente ordenado
 	 */
-	public static <T extends Comparable <? super T>> T kesimoRec(T[] V, int izq, int der, int k) {
-		while (izq != k || der != k) {
-			int med = OrdenacionRapida.partir(V, V[izq], izq, der);
+	public static <T extends Comparable <? super T>> T kesimoRec(T[] V, int inf, int sup, int k) {
+		while (inf != k || sup != k) {
+			// Se ordena el sub-vector entre los límites inferior y superior
+			// en base al pivote, que será el primer elemento del sub-vector
+			int piv = OrdenacionRapida.partir(V, V[inf], inf, sup);
 
-			if (k > med) {		// Debe buscar en el intervalo superior al pivote
-				izq = med+1;
+			if (k > piv) {
+				inf = piv+1;	// Debe buscar en el intervalo superior al pivote
 
-			} else {			// Debe buscar en el intervalo inferior al pivote
-				der = med;
+			} else {
+				sup = piv;		// Debe buscar en el intervalo inferior al pivote
 			}
 		}
 
