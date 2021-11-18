f4bbe0ca6c58aa605264eade0d133037bf1ba4f3
15Galan <antoniogalanG15@gmail.com>
2021-11-18 14:22:07 +0100

    OrdenacionRapidaBarajada.java analizado
    Se han modificado y añadido comentarios para
    explicar el funcionamiento del algoritmo en el código.

--- Prácticas/P2 - Ordenación Rápida/Proyecto/src/OrdenacionRapidaBarajada.java
+++ Prácticas/P2 - Ordenación Rápida/Proyecto/src/OrdenacionRapidaBarajada.java
@@ -8,14 +8,15 @@
 
 
 /**
- * Ordena un vector de elementos de forma ascendente.
+ * Ordena un vector ascendentemente cuyos elementos han sido aleatorizados.
  *
  * @author 		Antonio J. Galán Herrera
  */
 public class OrdenacionRapidaBarajada extends OrdenacionRapida {
 
     /**
-     * Permuta aleatoriamente los elementos de un vector y lo ordena ascendentemente
+     * Baraja aleatoriamente los elementos de un vector y luego los ordena ascendentemente.
+     *
      * Este método sirve para comparar tiempos experimentalmente.
      *
      * @param V     Vector a reordenar
@@ -27,13 +28,14 @@ public class OrdenacionRapidaBarajada extends OrdenacionRapida {
     }
 
     /**
-     * Reordena aleatoriamente los datos de un vector
+     * Baraja aleatoriamente los elementos de un vector.
      *
-     * @param V Vector a reordenar
-     * @param <T> Tipo del vector
+     * @param V     Vector a barajar
+     * @param <T>   Tipo del vector
      */
     private static <T> void barajar(T[] V) {
         for (int i = V.length-1; i > 0; i--) {
+            // Baraja la posición de un elemento con una posición anterior aleatoria
             intercambiar(V, i, aleat.nextInt(i+1));
         }
     }
