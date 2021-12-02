38f9984f9b40c4de332258fe962947bb2475a7b7
Galán <antoniogalanG15@gmail.com>
2021-12-03 00:58:07 +0100

    Práctica 3: Algoritmo Voraz implementado
    Se ha usado únicamente GitHub Copilot, pero da una implementación correcta.
    Debe adaptarse.

--- Prácticas/P3 - Problema de la Mochila/Proyecto/src/MochilaAV.java
+++ Prácticas/P3 - Problema de la Mochila/Proyecto/src/MochilaAV.java
@@ -1,18 +1,34 @@
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
+
 
 /**
- * 
- * @author ***** Indicar aqui el autor de la practica *******
+ * Clase que representa una resolución de un problema de la mochila, mediante un algoritmo voraz.
  *
+ * @author Antonio J. Galán Herrera
+ * @author Github Copilot
  */
 public class MochilaAV extends Mochila {
 
-	public SolucionMochila resolver(ProblemaMochila pm) {
-		SolucionMochila sm=null;
-		// A resolver por el alumno
-		return sm;
-	}
+	// Resuelve el problema de la mochila utilizando un algoritmo voraz.
+	// Recibe:		un problema ProblemaMochila con ítems y capacidad de la mochila.
+	// Devuelve: 	una solución SolucionMochila con los ítems de la mochila.
+    public SolucionMochila resolver(ProblemaMochila problema) {
+        ArrayList<Integer> solucion = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>(problema.getItems());
+        int capacidad = problema.getPesoMaximo();
+        int peso = 0;
+        int valor = 0;
+
+        // Se recorren los ítems de mayor a menor valor/peso.
+        for (Item item : items) {
+            // Si el ítem cabe en la mochila, se añade.
+            if (item.peso + peso <= capacidad) {
+                solucion.add(item.index);
+                peso += item.peso;
+                valor += item.valor;
+            }
+        }
+
+        return new SolucionMochila(solucion, valor, peso);
+    }
 }
