1dbe899008789695f2cca50fe9685955d20bde47
15Galan <antoniogalanG15@gmail.com>
2021-12-16 20:54:07 +0100

    Práctica 3: Algoritmo Voraz modificado
    Se ha corregido el código y ahora la complejidad es NLOG(N)
    como se especifica en el test de SIETTE.

--- Prácticas/P3 - Problema de la Mochila/Proyecto/src/MochilaAV.java
+++ Prácticas/P3 - Problema de la Mochila/Proyecto/src/MochilaAV.java
@@ -1,34 +1,72 @@
-import java.util.ArrayList;
-
-
 /**
  * Clase que representa una resolución de un problema de la mochila, mediante un algoritmo voraz.
  *
- * @author Antonio J. Galán Herrera
- * @author Github Copilot
+ * @author Antonio J. Galán Herrera, Rocío Montalvo Lafuente
  */
 public class MochilaAV extends Mochila {
 
-	// Resuelve el problema de la mochila utilizando un algoritmo voraz.
-	// Recibe:		un problema ProblemaMochila con ítems y capacidad de la mochila.
-	// Devuelve: 	una solución SolucionMochila con los ítems de la mochila.
+    /**
+     * Resolver el problema de la mochila usando un algoritmo voraz.
+     *
+     * @param problema		Problema de la mochila
+     * @return				Solución del problema
+     */
     public SolucionMochila resolver(ProblemaMochila problema) {
-        ArrayList<Integer> solucion = new ArrayList<>();
-        ArrayList<Item> items = new ArrayList<>(problema.getItems());
-        int capacidad = problema.getPesoMaximo();
-        int peso = 0;
-        int valor = 0;
+        // Ordenar los items de mayor a menor densidad mediante una función lambda
+        problema.items.sort(
+            (item1, item2) -> {
+                double densidad1 = ((double)item1.valor)/((double)item1.peso);
+                double densidad2 = ((double)item2.valor)/((double)item2.peso);
+                int res;
 
-        // Se recorren los ítems de mayor a menor valor/peso.
-        for (Item item : items) {
-            // Si el ítem cabe en la mochila, se añade.
-            if (item.peso + peso <= capacidad) {
-                solucion.add(item.index);
+                // Sustituir esta secuencia por 'Double.compare(densidad2, densidad1)' hace que
+                // falle la complejidad del test de SIETTE, pese a la recomendación de IntelliJ
+                if (densidad1 > densidad2) {
+                    res = -1;
+                } else if (densidad1 < densidad2) {
+                    res = 1;
+                } else {
+                    res = 0;
+                }
+
+                return res;
+            }
+        );
+
+        // Los ítems deben ordenarse previamente para un funcionamiento correcto del algoritmo
+        return algoritmoVoraz(problema);
+    }
+
+    // -----------------------------------------------------------------
+
+    /**
+     * Calcula (aproximadamente) la mejor solución del problema indicado.
+     *
+     * @param problema  Problema de la mochila
+     * @return          Solución del problema
+     */
+    private SolucionMochila algoritmoVoraz(ProblemaMochila problema) {
+        // Datos del problema
+        int capacidad   = problema.getPesoMaximo();
+
+        // Datos para el algoritmo
+        int[] solucion  = new int[problema.items.size()];
+        int peso        = 0;
+        int valor       = 0;
+
+        // Introducir los ítems en la mochila mientras quepan
+        for (Item item : problema.items) {
+            int i = item.index;
+            int unidades = item.unidades;
+
+            // Introducir tantas unidades de ese ítem mientras quepan
+            while (0 <= capacidad - peso - item.peso && solucion[i] < unidades) {
+                solucion[i]++;
                 peso += item.peso;
                 valor += item.valor;
             }
         }
 
-        return new SolucionMochila(solucion, valor, peso);
+        return new SolucionMochila(solucion, peso, valor);
     }
 }
