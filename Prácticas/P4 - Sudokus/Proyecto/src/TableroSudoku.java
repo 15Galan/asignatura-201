93d2a37548644f40917e6b91c472b2d5cb07f6cd
Galán <antoniogalanG15@gmail.com>
2021-12-24 16:55:35 +0100

    Práctica 4: código reestructurado
    Se ha limpiado y comentado el código a completar.

--- Prácticas/P4 - Sudokus/Proyecto/src/TableroSudoku.java
+++ Prácticas/P4 - Sudokus/Proyecto/src/TableroSudoku.java
@@ -1,162 +1,278 @@
-// ALUMNO:
-// GRUPO: 
-
 import java.util.*;
 
 
+/**
+ * Clase que representa el estado de un tablero de sudoku.
+ *
+ * @author Antonio J. Galán Herrera, Github Copilot
+ */
 public class TableroSudoku implements Cloneable {
 	
-	// constantes relativas al n� de filas y columnas del tablero
-	protected static final int MAXVALOR=9; 
-	protected static final int FILAS=9; 
-	protected static final int COLUMNAS=9; 
+	// Datos del estado del tablero
+	protected static final int MAXVALOR = 9;
+	protected static final int FILAS	= 9;
+	protected static final int COLUMNAS	= 9;
 							 
 	protected static Random r = new Random();
 	
-	protected int celdas[][]; // una celda vale cero si est\u00E1 libre.
-	
-	public TableroSudoku() {
-		celdas = new int[FILAS][COLUMNAS]; //todas a cero.
+	protected int[][] celdas;
+
+
+	/**
+	 * Ejecuta la resolución de un tablero de sudoku.
+	 *
+	 * @param args	Argumentos de clase recibidos
+	 */
+	public static void main(String[] args) {
+		String configuracion = ".4....36263.941...5.7.3.....9.3751..3.48.....17..62...716.9..2...96.......312..9.";
+		TableroSudoku sudoku = new TableroSudoku(configuracion);
+		List<TableroSudoku> tableros = sudoku.resolverTodos();
+
+		System.out.println(sudoku);
+		System.out.println(tableros.size());
+
+		for (TableroSudoku tablero : tableros) {
+			System.out.println(tablero);
+		}
 	}
 
-	// crea una copia de su par\u00E1metro
-	public TableroSudoku(TableroSudoku uno) {
-		TableroSudoku otro = (TableroSudoku) uno.clone();
+
+	// -----------------------------------------------------------------
+	// Constructores
+	// -----------------------------------------------------------------
+
+	/**
+	 * Crea un tablero con todas sus celdas a 0.
+	 */
+	public TableroSudoku() {
+		celdas = new int[FILAS][COLUMNAS];
+	}
+
+	/**
+	 * Constructor que crea un tablero haciendo una copia de otro.
+	 *
+	 * @param tablero	Tablero a copiar
+	 */
+	public TableroSudoku(TableroSudoku tablero) {
+		TableroSudoku otro = (TableroSudoku) tablero.clone();
 		this.celdas = otro.celdas;
 	}
 
-	// crear un tablero a parir de una configuraci\u00D3n inicial (las celdas vac\u00EDas
-	// se representan con el caracter ".".
-    public TableroSudoku(String s) {
+	/**
+	 * Crea un tablero a partir de una configuración inicial.
+	 *
+	 * @param configuracion		Cadena de caracteres que representa el estado de un tablero
+	 */
+    public TableroSudoku(String configuracion) {
     	this();
-    	if(s.length() != FILAS*COLUMNAS) {
-    		throw new RuntimeException("Construcci\u00D3n de sudoku no v\u00E1lida.");
+
+    	if(configuracion.length() == FILAS*COLUMNAS) {
+			for(int f = 0; f < FILAS; f++) {
+				for (int c = 0; c < COLUMNAS; c++) {
+					char ch = configuracion.charAt(f*FILAS + c);
+					celdas[f][c] = (Character.isDigit(ch) ? Integer.parseInt(Character.toString(ch)) : 0);
+				}
+			}
+
     	} else {
-    		for(int f=0;f<FILAS;f++) 
-				for(int c=0;c<COLUMNAS;c++) {
-					Character ch = s.charAt(f*FILAS+c);
-					celdas[f][c] = (Character.isDigit(ch) ? Integer.parseInt(ch.toString()) : 0 ); 
-				}		
+			throw new RuntimeException("Construcción de sudoku no válida.");
 		}		
     }
 
-	
-	/* Realizar una copia en profundidad del objeto
-	 * @see java.lang.Object#clone()
+
+	// -----------------------------------------------------------------
+	// Métodos auxiliares
+	// -----------------------------------------------------------------
+
+	/**
+	 * Resuelve un tablero de sudoku.
+	 *
+	 * @return	Lista con todos los tableros generados resueltos
 	 */
-	public Object clone()  {
-		TableroSudoku clon;
-		try {
-			clon = (TableroSudoku) super.clone();
-			clon.celdas = new int[FILAS][COLUMNAS]; 
-			for(int i=0; i<celdas.length; i++)
-				System.arraycopy(celdas[i], 0, clon.celdas[i], 0, celdas[i].length);
-		} catch (CloneNotSupportedException e) {
-			clon = null;
-		}	
-		return clon;
+	public List<TableroSudoku> resolverTodos() {
+		List<TableroSudoku> soluciones  = new LinkedList<>();
+		resolverTodos(soluciones, 0, 0);
+		return soluciones;
 	}
-	
-	/* Igualdad para la clase
-	 * @see java.lang.Object#equals()
+
+	/**
+	 * Resuelve un tablero de sudoku.
+	 *
+	 * @param soluciones	Lista con todos los tableros generados resueltos
+	 * @param fila			Fila de la celda a comprobar
+	 * @param columna		Columna de la celda a comprobar
 	 */
-	public boolean equals(Object obj) {
-		if (obj instanceof TableroSudoku) {
-			TableroSudoku otro = (TableroSudoku) obj;
-			for(int f=0; f<FILAS; f++)
-				if(!Arrays.equals(this.celdas[f],otro.celdas[f]))
-					return false;
-			return true;		
-		} else
-			return false;
-	}
-	
-
-
-	public String toString() {
-		String s = "";
-
-		for(int f=0;f<FILAS;f++) {
-			for(int c=0;c<COLUMNAS;c++) 
-				s += (celdas[f][c]==0 ? "." : String.format("%d",celdas[f][c])); 
-		}
-		return s;	
+	protected void resolverTodos(List<TableroSudoku> soluciones, int fila, int columna) {
+		// TODO
 	}
 
-
-	// devuelva true si la celda del tablero dada por fila y columna est\u00E1 vac\u00EDa.
+	/**
+	 * Indica si una celda está vacía.
+	 *
+	 * @param fila		Fila de la celda a comprobar
+	 * @param columna	Columna de la celda a comprobar
+	 *
+	 * @return	TRUE si la celda está vacía; FALSE en caso contrario
+	 */
 	protected boolean estaLibre(int fila, int columna) {
 		return celdas[fila][columna] == 0;
 	}
-	
-	// devuelve el n�mero de casillas libres en un sudoku.
+
+	/**
+	 * Cuenta el número de casillas libres en el tablero.
+	 *
+	 * @return	El número de casillas libres en el tablero.
+	 */
 	protected int numeroDeLibres() {
-		int n=0;
-	    for (int f = 0; f < FILAS; f++) 
-	        for (int c = 0; c < COLUMNAS; c++)
-	        	if(estaLibre(f,c))
-	        		n++;
-	    return n;
+		int libres = 0;
+
+		// Recorrer el tablero contando las celdas con 0.
+	    for (int f = 0; f < FILAS; f++) {
+			for (int c = 0; c < COLUMNAS; c++) {
+				if (estaLibre(f, c)) {
+					libres++;
+				}
+			}
+		}
+
+	    return libres;
 	}
-	
+
+	/**
+	 * Cuenta el número de casillas con un valor en el tablero.
+	 *
+	 * @return	El número de casillas con un valor en el tablero.
+	 */
 	protected int numeroDeFijos() {
 		return FILAS*COLUMNAS - numeroDeLibres();
 	}
 
-	// Devuelve true si @valor ya esta en la fila @fila.
+	/**
+	 * Comprueba si un valor ya está en una fila.
+	 *
+	 * @param fila		Fila a comprobar
+	 * @param valor		Valor a comprobar
+	 *
+	 * @return TRUE si el valor ya está en la fila; FALSE en caso contrario.
+	 */
 	protected boolean estaEnFila(int fila, int valor) {
-		// A completar por el alumno
+		// TODO
 		return false;
-	}    
+	}
 
-	// Devuelve true si @valor ya esta en la columna @columna.
+	/**
+	 * Comprueba si un valor ya está en una columna.
+	 *
+	 * @param columna 	Columna a comprobar
+	 * @param valor 	Valor a comprobar
+	 *
+	 * @return TRUE si el valor ya está en la columna; FALSE en caso contrario.
+	 */
 	protected boolean estaEnColumna(int columna, int valor) {
-		// A completar por el alumno
+		// TODO
 		return false;
-	}    
-	
+	}
 
-	// Devuelve true si @valor ya esta en subtablero al que pertence @fila y @columna.
+	/**
+	 * Comprueba si un valor ya está en un subtablero.
+	 *
+	 * @param fila 		Fila de la celda a comprobar
+	 * @param columna 	Columna de la celda a comprobar
+	 * @param valor 	Valor a comprobar
+	 *
+	 * @return TRUE si el valor ya está en el subtablero; FALSE en caso contrario.
+	 */
 	protected boolean estaEnSubtablero(int fila, int columna, int valor) {
-		// A completar por el alumno	
+		// TODO
 		return false;		
-	}    
+	}
 
-	
-	// Devuelve true si se puede colocar el @valor en la @fila y @columna dadas.
+	/**
+	 * Comprueba si es posible colocar un valor en una celda.
+	 * Es decir, si el valor no está en la fila, la columna o el subtablero.
+	 *
+	 * @param fila 		Fila de la celda a comprobar
+	 * @param columna 	Columna de la celda a comprobar
+	 * @param valor 	Valor a comprobar
+	 *
+	 * @return TRUE si se puede colocar el valor en la celda; FALSE en caso contrario.
+	 */
 	protected boolean sePuedePonerEn(int fila, int columna, int valor) {
-		// A completar por el alumno
+		// TODO
 		return false;
 	}
-	
-	
-	
 
-	protected void resolverTodos(List<TableroSudoku> soluciones, int fila, int columna) {
-		// A completar por el alumno
-	}
-	
 
-	public List<TableroSudoku> resolverTodos() {
-        List<TableroSudoku> sols  = new LinkedList<TableroSudoku>();
-        resolverTodos(sols, 0, 0);
-		return sols;
-	}
-	
-	
-	public static void main(String arg[]) {
-		TableroSudoku t = new TableroSudoku( 
-			    ".4....36263.941...5.7.3.....9.3751..3.48.....17..62...716.9..2...96.......312..9.");
-		List<TableroSudoku> lt = t.resolverTodos();
-		System.out.println(t);
-		System.out.println(lt.size());
-		for(Iterator<TableroSudoku> i= lt.iterator(); i.hasNext();) {
-			TableroSudoku ts = i.next(); 
-			System.out.println(ts);
-			
+	// -----------------------------------------------------------------
+	// Métodos sobreescritos
+	// -----------------------------------------------------------------
+
+	/**
+	 * Realiza una copia en profundidad del objeto.
+	 * @see java.lang.Object#clone()
+	 *
+	 * @return	Una copia del objeto
+	 */
+	@Override
+	public Object clone()  {
+		TableroSudoku clon;
+
+		try {
+			clon = (TableroSudoku) super.clone();
+			clon.celdas = new int[FILAS][COLUMNAS];
+
+			for(int i = 0; i < celdas.length; i++) {
+				System.arraycopy(celdas[i], 0, clon.celdas[i], 0, celdas[i].length);
+			}
+
+		} catch (CloneNotSupportedException e) {
+			clon = null;
 		}
 
+		return clon;
 	}
-	
-	
+
+	/**
+	 * Establece las condiciones de igualdad para 2 instancias de esta clase.
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 *
+	 * @return TRUE si la instancia recibida es igual; FALSE en caso contrario
+	 */
+	@Override
+	public boolean equals(Object objeto) {
+		// Comprobar que el objeto recibido es un TableroSudoku
+		if (objeto instanceof TableroSudoku otro) {
+
+			// Comprobar que las filas de ambos tableros son iguales
+			for(int f = 0; f < FILAS; f++) {
+				if (!Arrays.equals(this.celdas[f], otro.celdas[f])) {
+					return false;
+				}
+			}
+
+			return true;
+
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Genera una representación del estado del tablero.
+	 * El resultado es una configuración del tablero.
+	 *
+	 * @return	Representación del estado del tablero
+	 */
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+
+		for(int f = 0; f < FILAS; f++) {
+			for(int c = 0; c < COLUMNAS; c++) {
+				s.append(celdas[f][c] == 0 ? "." : String.format("%d", celdas[f][c]));
+			}
+		}
+
+		return s.toString();
+	}
 }
