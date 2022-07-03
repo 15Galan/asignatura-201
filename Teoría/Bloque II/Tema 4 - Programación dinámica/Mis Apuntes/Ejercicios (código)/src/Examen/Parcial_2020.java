789a9cd1c49d012145586987f428470dedb1c197
Galán <antoniogalanG15@gmail.com>
2022-07-03 21:13:35 +0100

    Tema 4: parcial 2 del 2020 modificado
    Problema de las Entidades Financieras.
    Se aplicó la misma estructura que el resto de ejercicios.

--- Teoría/Bloque II/Tema 4 - Programación dinámica/Mis Apuntes/Ejercicios (código)/src/Examen/Parcial_2020.java
+++ Teoría/Bloque II/Tema 4 - Programación dinámica/Mis Apuntes/Ejercicios (código)/src/Examen/Parcial_2020.java
@@ -14,17 +14,33 @@ import java.util.Arrays;
  * Se pretende distribuir 'M' euros entre las 'n' entidades
  * de forma que el interés total obtenido sea máximo.
  *
+ *
  * @author Antonio J. Galán Herrera
  */
 public class Parcial_2020 {
 
+    // NOTA referente al parcial 2018-2019 (mismo ejercicio):
+    // El de 2020-2021 ya incluye M = 0 en el ejemplo.
+
+    /*
+    Nótese que la primera columna es 0.0, correspondiente a un interés de M = 0 euros.
+
+    Esta columna se utiliza para facilitar el proceso de programación dinámica, y se llega a
+    esta conclusión tras intentar rellenar la tabla 'A' por primera vez, como un caso base;
+    ya que como la tabla A maneja el caso base de M = 0, entonces también debería haber un
+    interés con 0; y es necesario que la tabla A tenga un caso 0, porque requiere repartir
+    el dinero y es posible que se repartan 0 euros a una entidad.
+
+    Por tanto, debe haber un interés para M = 0 y por lógica, lo normal es que sea 0.0.
+    */
+
     // Datos del problema
-    private static double[][] interes;  // Tabla que representa el interés anual de cada banco
-    private static int M;               // Cantidad de euros a depositar
-    private static int n;               // Número de bancos
+    private static double[][] I;    // Tabla que representa el interés anual de cada banco
+    private static int n;           // Número de bancos
+    private static int M;           // Cantidad de euros a depositar
 
     // Datos del desarrollo
-    private static double[][] A;        // Tabla utilizada en el proceso de programación dinámica
+    private static double[][] A;    // Tabla utilizada en el proceso de programación dinámica
 
 
     /**
@@ -34,49 +50,35 @@ public class Parcial_2020 {
      */
     public static void main(String[] args) {
         // Datos del problema
-        interes = new double[][] {  // Ejemplo del parcial
-                {0.0, 0.3, 0.4, 0.6, 0.8, 0.9, 1.0, 1.0},  // Entidad 1
-                {0.0, 0.5, 0.6, 0.8, 1.0, 1.0, 1.0, 1.0},  // Entidad 2
-                {0.0, 0.4, 0.4, 0.5, 0.6, 0.7, 0.9, 1.0},  // Entidad 3
-                {0.0, 0.2, 0.4, 0.7, 0.7, 0.8, 0.8, 0.9}   // Entidad 4
+        I = new double[][] {    // Ejemplo del parcial
+            {0.0, 0.3, 0.4, 0.6, 0.8, 0.9, 1.0, 1.0},  // Entidad 1
+            {0.0, 0.5, 0.6, 0.8, 1.0, 1.0, 1.0, 1.0},  // Entidad 2
+            {0.0, 0.4, 0.4, 0.5, 0.6, 0.7, 0.9, 1.0},  // Entidad 3
+            {0.0, 0.2, 0.4, 0.7, 0.7, 0.8, 0.8, 0.9}   // Entidad 4
         };
 
-//        double interes = new double[][] {   // Ejemplo de Diego
-//                {0.0, 0.1, 0.2, 0.4, 0.6, 0.7},  // Entidad 1
-//                {0.0, 0.1, 0.3, 0.4, 0.5, 0.8},  // Entidad 2
-//                {0.0, 0.0, 0.2, 0.5, 0.7, 0.8}   // Entidad 3
-//        };
+        // I = new double[][] {    // Ejemplo de Diego
+        //     {0.0, 0.1, 0.2, 0.4, 0.6, 0.7},  // Entidad 1
+        //     {0.0, 0.1, 0.3, 0.4, 0.5, 0.8},  // Entidad 2
+        //     {0.0, 0.0, 0.2, 0.5, 0.7, 0.8}   // Entidad 3
+        // };
 
-
-        // NOTA: Referente al parcial 2018-2019 (mismo ejercicio).
-        // El de 2020-2021 ya incluye M = 0 en el ejemplo.
-
-        /*
-        Nótese que la primera columna es 0.0, correspondiente a un interés M = 0 euros.
-
-        Esta columna se utiliza para facilitar el proceso de programación dinámica, y se llega
-        a esta conclusión tras intentar rellenar la tabla 'A' por primera vez, ya que 'como la
-        tabla A maneja el caso base de M = 0, entonces también debería haber un interés con 0;
-        y es necesario que la tabla A tenga un caso 0, porque requiere repartir el dinero y es
-        posible que se repartan 0 euros a una entidad'.
-
-        Por tanto, debe haber un interés para M = 0 y por lógica, lo normal es que sea 0.0.
-        */
-
-        M = interes[0].length;  // Número de columnas
-        n = interes.length;     // Número de filas
+        M = I[0].length;    // Número de columnas
+        n = I.length;       // Número de filas
 
 
         // Mostrar los datos del problema
-        System.out.println("Se quiere repartir " + (M-1) + " euros entre " + n + " entidades.");
-        System.out.println("Los intereses siguen esta proporción (n\\M):\n\n" + mostrarTabla(interes));
+        System.out.println("Dinero: " + (M-1));     // Porque hay 1 columna más
+        System.out.println("Bancos: " + n);
+        System.out.println("Intereses:\n" + mostrarTabla(I));
 
 
         // Resolución
         rellenarTablaA();
 
-        System.out.println("El interés máximo obtenido es " + maximoBeneficio() + " euros.");
-        System.out.println("La distribución de euros en cada banco es " + Arrays.toString(eurosPorBanco()) + ".");
+        System.out.println("\nSolución:  " + Arrays.toString(solucion()));
+        System.out.println("\tValor: " + valor());
+
 
         // Información adicional
         System.out.println("\n--- Información adicional ---");
@@ -107,8 +109,8 @@ public class Parcial_2020 {
      * Aplica la siguiente ecuación de Bellman para una fila y columna
      * concretas de la tabla 'A'.
      *
-     * A(i,j) = interés(i,j)                                   si  i = 0
-     * A(i,j) = MAX[0 <= k <= j](A(i-1,k) + interés(i,j-k))    si  i > 0
+     * A(i,j) = I(i,j)                                   si  i = 0
+     * A(i,j) = MAX[0 <= k <= j][A(i-1,k) + I(i,j-k)]    si  i > 0
      *
      * @param i     Fila de la tabla 'A'
      * @param j     Columna de la tabla 'A'
@@ -116,14 +118,14 @@ public class Parcial_2020 {
     private static void bellman(int i, int j) {
         // Caso base
         if (i == 0) {
-            A[i][j] = interes[i][j];
+            A[i][j] = I[i][j];
 
-        // Caso 'recursivo' (no se usa 'bellman()', sino 'A[][]')
+        // Caso iterativo
         } else {
             double max = 0.0;
 
             for (int k = 0; k <= j; k++) {
-                double aux = A[i-1][k] + interes[i][j-k];
+                double aux = A[i-1][k] + I[i][j-k];
 
                 if (max < aux) {
                     max = aux;
@@ -135,43 +137,18 @@ public class Parcial_2020 {
     }
 
     /**
-     * Calcula el interés máximo obtenido al repartir 'M' euros entre las 'n' entidades.
-     *
-     * @return  El valor de la tabla 'A' que representa el interés máximo
-     */
-    private static double maximoBeneficio() {
-        double max = 0.0;
-
-        for (int j = 0; j < M; j++) {
-            double aux = A[n-1][j];
-
-            if (max < aux) {
-                max = aux;
-            }
-        }
-
-        return max;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Funciones auxiliares
-    // ------------------------------------------------------------------------
-
-    /**
      * Calcula la cantidad de euros a invertir en cada banco.
      *
      * @return  Vector con la cantidad 'm' de euros a invertir en la entidad 'i'
      */
-    private static int[] eurosPorBanco() {
-        double[] distribucion = new double[n];  // Vector con la distribución de euros
-        int[] dinero = new int[n];              // Vector con la cantidad de euros a invertir en cada banco
-
-        int resto = M-1;                        // Se resta porque 'M' son las columnas
+    private static int[] solucion() {
+        int[] S = new int[n];           // Euros a invertir en cada banco
+        double[] D = new double[n];     // Interés óptimo de cada entidad
+        int resto = M-1;                // Cantidad restante de euros a depositar
 
         // Calcular el interés obtenido en cada entidad para la cantidad 'm' de euros
         for (int i = n-1; i >= 0; i--) {
-            distribucion[i] = A[i][resto];
+            D[i] = A[i][resto];
             resto -= A[i][resto];
         }
 
@@ -181,17 +158,31 @@ public class Parcial_2020 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < M; j++) {
                 // Si el interés está en la distribución, se han metido 'm' euros en la entidad 'i'
-                if (A[i][j] == distribucion[i]) {
-                    int m = j - ingresado;          // 'j' euros del total menos los ya ingresados
+                if (A[i][j] == D[i]) {
+                    int m = j - ingresado;  // 'j' euros del total menos los ya ingresados
 
-                    dinero[i] = m;
-                    ingresado = j;                  // Se actualiza el total de ingresos
+                    S[i] = m;
+                    ingresado = j;          // Se actualiza el total de ingresos
                 }
             }
         }
 
-        return dinero;
+        return S;
     }
+
+    /**
+     * Calcula el interés máximo obtenido al repartir 'M' euros entre las 'n' entidades.
+     *
+     * @return  El valor de la tabla 'A' que representa el interés máximo
+     */
+    private static double valor() {
+        return A[n-1][M-1];
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Funciones auxiliares
+    // ------------------------------------------------------------------------
 
     /**
      * Genera una representación de una tabla con sus índices como cabeceras.
